07269b5f2cb3b7afe93a7277ca9443f1cd143e8b
Manobala <manobalaarul3@gmail.com>
2025-07-19 18:17:39 +0530

    Init

--- src/App.jsx
+++ src/App.jsx
@@ -7,16 +7,11 @@ const App = () => {
   const [isCollapsed, setIsCollapsed] = useState(false);
 
   return (
-    <div className="flex h-screen">
-      <Sidebar isCollapsed={isCollapsed} setIsCollapsed={setIsCollapsed} />
-      <div className="flex-1 flex flex-col">
-        <Navbar toggleSidebar={() => setIsCollapsed(!isCollapsed)} />
-        <main className="p-4">
-          <Outlet/>
-        </main>
-      </div>
+    <div>
+      <Sidebar/>
+      <Outlet/>
     </div>
-  );
+  )
 }
 
 export default App
